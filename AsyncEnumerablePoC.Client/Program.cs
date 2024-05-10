feb739544f92b65b78b2a738d4ebf7395197fb17
Bartosz Kuriata <bak@scada-international.com>
2024-05-11 01:22:55 +0200

    Uncomment Program.cs

--- AsyncEnumerablePoC.Client/Program.cs
+++ AsyncEnumerablePoC.Client/Program.cs
@@ -5,10 +5,10 @@ using BenchmarkDotNet.Running;
 var config = DefaultConfig.Instance
     .WithOption(ConfigOptions.JoinSummary, true);
 
-//BenchmarkRunner.Run<HistoricalDataBenchmarkDbReadOnly>(config);
-//BenchmarkRunner.Run<HistoricalDataBenchmarkDbReadTransformedTwice>(config);
-//BenchmarkRunner.Run<HistoricalDataBenchmarkGetData>(config);
-//BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformOnceAndSave>(config);
-//BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformTwiceAndSave>(config);
-//BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformThreeAndSave>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkDbReadOnly>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkDbReadTransformedTwice>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkGetData>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformOnceAndSave>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformTwiceAndSave>(config);
+BenchmarkRunner.Run<HistoricalDataBenchmarkGetDataTransformThreeAndSave>(config);
 BenchmarkRunner.Run<HistoricalDataComplexBenchmark>(config);
